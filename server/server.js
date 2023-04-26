a4cf511d4391430a15a3c7d3262a88634b81a460
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-26 12:42:46 -0400

    updated login to use GraphQL (maybe)

--- server/server.js
+++ server/server.js
@@ -2,17 +2,27 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const {ApolloServer} = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(authMiddleware)
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
 
 app.use(routes);
 
