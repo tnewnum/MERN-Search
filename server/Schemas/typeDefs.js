f9787f9bc4d8f7e5337179ecc01fefe6eec656fd
Thomas Newnum <thomas.newnum@gmail.com>
2023-04-29 12:00:48 -0400

    server launches now

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -1,10 +1,41 @@
-const { gql } = require('apollo-server-express');
-
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-  type Query {
-    hello: String
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
   }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Query {
+    users: [User]
+    books: [Book]
+    me: User
+  }
+
+  # type Auth {
+  #   token: ID!
+  #   user: User
+  # }
+
+
+  # type Mutation {
+  #   login(email: String!, password: String!): Auth
+  #   addUser(username:String!, email: String!, password: String!): Auth
+  #   saveBook(input: BookInput!): User
+  #   removeeBook(bookId: String!): User
+  # }
 `;
 
 module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,23 +1,13 @@
 const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
-const typeDefs = require("./Schemas/typeDefs");
-const resolvers = require("./Schemas/resolvers");
+const { typeDefs, resolvers } = require("./Schemas");
+
+const path = require("path");
+const db = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(authMiddleware);
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
 
 const server = new ApolloServer({
   typeDefs,
@@ -25,7 +15,13 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
